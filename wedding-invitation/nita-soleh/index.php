1ac535adf540ad71dd6e9d635b98275a66ce98e4
fiqriashiddiqi <fiqriashiddiqi@gmail.com>
2021-10-12 21:40:39 +0700

    pus

--- wedding-invitation/nita-soleh/index.php
+++ wedding-invitation/nita-soleh/index.php
@@ -208,7 +208,7 @@ jQuery(function($){
 					
 					<div class="et_pb_blurb_container">
 						<h4 class="et_pb_module_header"><span>Nita & Soleh</span></h4>
-						<div class="et_pb_blurb_description"><p style="text-align: center;">19 September 2021</p></div>
+						<div class="et_pb_blurb_description"><p style="text-align: center;">19 Oktober 2021</p></div>
 
 						<a class="et_pb_button et_pb_button_1 et_animated et_pb_bg_layout_light" href="https://calendar.google.com/calendar/ical/lk5bkhd1jfddajg9d1aropueu0%40group.calendar.google.com/public/basic.ics" target="_blank" style="background-color: #C89F84;width: 50%;margin-left: auto;margin-right: auto;margin-top: 20px; text-align: center;">Save Date</a>
 					</div>
